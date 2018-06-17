66f1602eeb61a9f67e4e69b59d73c41fbe61965c
Melih Gültekin <mmelihgultekin@gmail.com>
2018-06-17 12:38:55 +0300

    adds initial project

---
+++ app/src/main/java/com/projects/melih/wonderandwander/MainActivity.java
@@ -0,0 +1,16 @@
+package com.projects.melih.wonderandwander;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by Melih Gültekin on 15.06.2018
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
