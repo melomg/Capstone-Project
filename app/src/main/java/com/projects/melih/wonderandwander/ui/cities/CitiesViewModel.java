a5116973a0ca3f30a4a9ef59da45c767ac3fadec
Melih Gültekin <mmelihgultekin@gmail.com>
2018-06-23 04:52:22 +0300

    feat: adds initial city respository

--- app/src/main/java/com/projects/melih/wonderandwander/ui/cities/CitiesViewModel.java
+++ app/src/main/java/com/projects/melih/wonderandwander/ui/cities/CitiesViewModel.java
@@ -1,9 +1,54 @@
 package com.projects.melih.wonderandwander.ui.cities;
 
 import android.arch.lifecycle.ViewModel;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.projects.melih.wonderandwander.repository.CitiesRepository;
+import com.projects.melih.wonderandwander.repository.remote.DataCallback;
+import com.projects.melih.wonderandwander.repository.remote.ErrorState;
+import com.projects.melih.wonderandwander.repository.remote.response.ResponseCities;
+
+import javax.inject.Inject;
+
+import retrofit2.Call;
+import timber.log.Timber;
 
 /**
  * Created by Melih Gültekin on 18.06.2018
  */
 public class CitiesViewModel extends ViewModel {
+
+    private final CitiesRepository citiesRepository;
+    private Call<ResponseCities> callCities;
+
+    @SuppressWarnings("WeakerAccess")
+    @Inject
+    public CitiesViewModel(CitiesRepository citiesRepository) {
+        this.citiesRepository = citiesRepository;
+    }
+
+    public void search(@NonNull String cityName) {
+        if (callCities != null) {
+            callCities.cancel();
+        }
+        callCities = citiesRepository.fetchCitiesFromNetwork(cityName, new DataCallback<ResponseCities>() {
+            @Override
+            public void onComplete(@Nullable ResponseCities data, @Nullable ErrorState errorState) {
+                if (data != null) {
+                    Timber.d(data.toString());
+                } else {
+                    Timber.e("error");
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        if (callCities != null) {
+            callCities.cancel();
+        }
+    }
 }
