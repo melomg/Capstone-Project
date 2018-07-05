23c1d0e0bbf565fe7a7039c6302b3c96eee26918
Melih Gültekin <mmelihgultekin@gmail.com>
2018-07-05 21:24:46 +0300

    feat: adds user model class

---
+++ app/src/main/java/com/projects/melih/wonderandwander/model/User.java
@@ -0,0 +1,97 @@
+package com.projects.melih.wonderandwander.model;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Melih Gültekin on 03.07.2018
+ */
+@SuppressWarnings("unused")
+@Entity(tableName = "user")
+public class User {
+    @Nullable
+    private String displayName;
+
+    @Nullable
+    private String email;
+
+    @Nullable
+    private String phoneNumber;
+
+    @Nullable
+    private String photoUrl;
+
+    private boolean isAnonymous;
+
+    @PrimaryKey
+    private String uId;
+
+    @Ignore
+    public User() {
+    }
+
+    public User(@Nullable String displayName, @Nullable String email, @Nullable String phoneNumber,
+                @Nullable String photoUrl, @Nullable boolean isAnonymous, String uId) {
+        this.displayName = displayName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.photoUrl = photoUrl;
+        this.isAnonymous = isAnonymous;
+        this.uId = uId;
+    }
+
+    @Nullable
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(@Nullable String displayName) {
+        this.displayName = displayName;
+    }
+
+    @Nullable
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@Nullable String email) {
+        this.email = email;
+    }
+
+    @Nullable
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(@Nullable String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Nullable
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(@Nullable String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public boolean isAnonymous() {
+        return isAnonymous;
+    }
+
+    public void setAnonymous(boolean anonymous) {
+        isAnonymous = anonymous;
+    }
+
+    @Nullable
+    public String getuId() {
+        return uId;
+    }
+
+    public void setuId(@Nullable String uId) {
+        this.uId = uId;
+    }
+}
