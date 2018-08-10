a240d25f3fd98db7b2499dfc2163f25404deb44c
Melih Gültekin <mmelihgultekin@gmail.com>
2018-08-10 21:56:37 +0300

    create config.yml

---
+++ .circleci/ci-scripts/ci-mock-google-services-setup.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+export APP_MODULE_PATH="app"
+export JSON_PATH="$APP_MODULE_PATH/google-services.json"
+
+if [ ! -e ${JSON_PATH} ]; then
+    echo "Writing mock google-services.json file to $JSON_PATH..."
+
+    echo '{
+      "project_info": {
+        "project_number": "012345678912",
+        "firebase_url": "https://wonderandwander-mock.firebaseio.com",
+        "project_id": "wonderandwander-mock",
+        "storage_bucket": "wonderandwander-mock.appspot.com"
+      },
+      "client": [
+        {
+          "client_info": {
+            "mobilesdk_app_id": "1:XXXXXXXXXXXXX:android:XXXXXXXXXXXX",
+            "android_client_info": {
+              "package_name": "com.projects.melih.wonderandwander.free"
+            }
+          },
+          "oauth_client": [
+            {
+              "client_id": "XXXXXXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.apps.googleusercontent.com",
+              "client_type": 1,
+              "android_info": {
+                "package_name": "com.projects.melih.wonderandwander.free",
+                "certificate_hash": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+              }
+            },
+            {
+              "client_id": "XXXXXXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.apps.googleusercontent.com",
+              "client_type": 1,
+              "android_info": {
+                "package_name": "com.projects.melih.wonderandwander.free",
+                "certificate_hash": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+              }
+            },
+            {
+              "client_id": "XXXXXXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.apps.googleusercontent.com",
+              "client_type": 3
+            },
+            {
+              "client_id": "XXXXXXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.apps.googleusercontent.com",
+              "client_type": 3
+            }
+          ],
+          "api_key": [
+            {
+              "current_key": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+            }
+          ],
+          "services": {
+            "appinvite_service": {
+              "status": 1,
+              "other_platform_oauth_client": []
+            },
+            "ads_service": {
+              "status": 2
+            }
+          }
+        }
+      ],
+      "configuration_version": "1"
+    }' > ${JSON_PATH}
+
+    echo "Done."
+else
+    echo "The $JSON_PATH file already exists, skipping..."
+fi
---
+++ .circleci/ci-scripts/circleci_env_setup.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+export KEYSTORE_PROPERTIES=$HOME"/wonderandwander/keystore.properties"
+export STORE_FILE_LOCATION=$HOME"/wonderandwander/${STORE_FILE}"
+
+function copyEnvVarsToProperties {
+
+    echo "Keystore Properties should exist at $KEYSTORE_PROPERTIES"
+
+    if [ ! -f "$KEYSTORE_PROPERTIES" ]
+    then
+        echo "${KEYSTORE_PROPERTIES} does not exist...Creating file"
+
+        touch ${KEYSTORE_PROPERTIES}
+
+        echo "keyAlias=${KEY_ALIAS}" >> ${KEYSTORE_PROPERTIES}
+        echo "keyPassword=${KEY_PASSWORD}" >> ${KEYSTORE_PROPERTIES}
+        echo "storeFile=${STORE_FILE_LOCATION}" >> ${KEYSTORE_PROPERTIES}
+        echo "storePassword ${STORE_PASSWORD}" >> ${KEYSTORE_PROPERTIES}
+    fi
+}
+
+
+# download key store file from remote location
+# keystore URI will be the location uri for the *.jks file for signing application
+function downloadKeyStoreFile {
+    # use curl to download a keystore from $KEYSTORE_URI, if set,
+    # to the path/filename set in $KEYSTORE.
+    echo "Looking for $STORE_FILE_LOCATION ..."
+
+    if [ ! -f ${STORE_FILE_LOCATION} ] ; then
+        echo "Keystore file is missing, performing download for ${STORE_FILE} at ${KEY_STORE_URI}"
+        # we're using curl instead of wget because it will not
+        # expose the sensitive uri in the build logs:
+        curl -L -o ${STORE_FILE} ${KEY_STORE_URI}
+    else
+            echo "Keystore uri not set.  .APK artifact will not be signed."
+    fi
+}
+
+# execute functions
+copyEnvVarsToProperties
+downloadKeyStoreFile
